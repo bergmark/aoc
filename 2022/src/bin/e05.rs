9bd10bc5d7a2cccdae6bfff4fee7d3ccc58696aa
Adam Bergmark <adam@bergmark.nl>
2022-12-05 08:13:10 +0100

    2022 05

---
+++ 2022/src/bin/e05.rs
@@ -0,0 +1,150 @@
+use aoc2022::*;
+use lazy_regex::regex;
+use std::collections::VecDeque;
+
+fn main() {
+    run()
+}
+
+#[test]
+fn test() {
+    run()
+}
+
+fn run() {
+    assert_eq!(a("txt/s05.txt"), "CMZ");
+    assert_eq!(a("txt/e05.txt"), "FCVRLMVQP");
+    assert_eq!(b("txt/s05.txt"), "MCD");
+    assert_eq!(b("txt/e05.txt"), "RWLWGJGFD");
+}
+
+fn a(s: &str) -> String {
+    let rows: Vec<Row> = read_parsed::<Row>(s).collect();
+    let mut stacks: Vec<Stack> = vec![Default::default(); 10];
+    let mut instructions: Vec<Move> = vec![];
+    for row in &rows {
+        if let Row::Buckets { v } = row {
+            let mut i = 0;
+            for bucket in v {
+                i += 1;
+                if let Some(c) = bucket {
+                    stacks[i].push_front(*c);
+                }
+            }
+        } else if let Row::Move(m) = row {
+            instructions.push(m.clone());
+        }
+    }
+    //dbg!(&stacks);
+    //dbg!(&instructions);
+
+    for Move { amount, src, dest } in instructions {
+        //println!("move {amount} from {src} to {dest}");
+        for _ in 1..=amount {
+            let crat = stacks[src].pop_back().unwrap();
+            stacks[dest].push_back(crat);
+        }
+        //dbg!(&stacks);
+    }
+
+    let mut res: Vec<char> = vec![];
+    for stack in stacks {
+        if !stack.is_empty() {
+            res.push(*stack.back().unwrap());
+        }
+    }
+
+    res.iter().collect()
+}
+
+fn b(s: &str) -> String {
+    let rows: Vec<Row> = read_parsed::<Row>(s).collect();
+    let mut stacks: Vec<Stack> = vec![Default::default(); 10];
+    let mut instructions: Vec<Move> = vec![];
+    for row in &rows {
+        if let Row::Buckets { v } = row {
+            let mut i = 0;
+            for bucket in v {
+                i += 1;
+                if let Some(c) = bucket {
+                    stacks[i].push_front(*c);
+                }
+            }
+        } else if let Row::Move(m) = row {
+            instructions.push(m.clone());
+        }
+    }
+    //dbg!(&stacks);
+    //dbg!(&instructions);
+
+    for Move { amount, src, dest } in instructions {
+        println!("move {amount} from {src} to {dest}");
+        let mut intermed = vec![];
+        for _ in 1..=amount {
+            let crat = stacks[src].pop_back().unwrap();
+            intermed.push(crat);
+        }
+        for i in intermed.iter().rev() {
+            stacks[dest].push_back(*i);
+        }
+        //dbg!(&stacks);
+    }
+
+    let mut res: Vec<char> = vec![];
+    for stack in stacks {
+        if !stack.is_empty() {
+            res.push(*stack.back().unwrap());
+        }
+    }
+
+    res.iter().collect()
+}
+
+type Stack = VecDeque<char>;
+
+#[derive(Debug, Clone)]
+enum Row {
+    Empty,
+    Buckets { v: Vec<Option<char>> },
+    Move(Move),
+}
+
+#[derive(Debug, Clone)]
+struct Move {
+    amount: usize,
+    src: usize,
+    dest: usize,
+}
+
+impl FromStr for Row {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, ()> {
+        if s.is_empty() {
+            Ok(Row::Empty)
+        } else if regex!(r#"^[ \d]+$"#).is_match(s) {
+            Ok(Row::Empty)
+        } else if s.starts_with("move") {
+            let r = regex!(r#"move (\d+) from (\d+) to (\d+)"#);
+            let cap = r.captures(s).unwrap();
+            let amount = cap.get(1).unwrap().as_str().parse::<usize>().unwrap();
+            let src = cap.get(2).unwrap().as_str().parse::<usize>().unwrap();
+            let dest = cap.get(3).unwrap().as_str().parse::<usize>().unwrap();
+            Ok(Row::Move(Move { amount, src, dest }))
+        } else {
+            let chunks = s.chars().chunks(4);
+            let mut res = vec![];
+            for chunk in &chunks {
+                let chunk: String = chunk.collect();
+                if regex!("^ +$").is_match(&chunk) {
+                    res.push(None);
+                } else if regex!(r#"^\[([A-Z])\]"#).is_match(&chunk) {
+                    let cap = regex!(r#"^\[([A-Z])\]"#).captures(&chunk).unwrap();
+                    res.push(Some(cap.get(1).unwrap().as_str().parse::<char>().unwrap()));
+                } else {
+                    panic!("foo: {chunk:?}")
+                }
+            }
+            Ok(Row::Buckets { v: res })
+        }
+    }
+}
